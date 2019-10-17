b98ca083a0a969dc9dcf4a46e95739a129a879ff
Shane Tremel <shanetremel24@yahoo.com>
2019-10-17 13:34:48 -0500

    Added basic dungeon layout
    There was also a bug where any text input gave the "You have this much gold" message, I put that println into an if statement so it only gives that output when you type shop

--- Main.java
+++ Main.java
@@ -1,38 +1,46 @@
 import java.util.Scanner;
+import java.lang.Math;
 public class Main{
     static Scanner input = new Scanner(System.in);
     static boolean RUNGAME = true;
     static boolean setClass = false;
     static String userInput = null;
     final static String PATH = "saveFile.csv";
-    
+    static boolean fightState = false;
+
+    static int numberOfRooms = 4;//(int)Math.floor(Math.random()*5+1);
+
     public static void main(String[] args){
         System.out.println("Welcome to Dungeon Masters Java.  This is a text-based RPG.  Would you like to load a save file?");
         userInput = getInput("Would you like to load a save file?");
         if(userInput.equalsIgnoreCase("yes")){userInput = "true";}
         hasSaveFile(Boolean.parseBoolean(userInput));
-       
+
         while (RUNGAME){
             System.out.println("You can type shop to visit the shop, or you can type dungeon to explore the dungeon.(type 'quit' to quit)");
             userInput = getInput("What do you want to do?");
             checkInput(userInput);
-            System.out.println("You have this much gold"+User.getGold());
+            RUNGAME = false;
+            if(userInput.equalsIgnoreCase("shop"))
+                System.out.println("You have this much gold"+User.getGold());
         }
     }
-    
+
     public static void hasSaveFile(boolean hasSaveFile){
         if(hasSaveFile){
             setClass = true;
             User user = SaveFileReader.readFromFile(PATH);;
         }
     }
+
     public static void trySetClass(){
-            while (!setClass){
+        while (!setClass){
             System.out.println("You can be a warrior, thief, or mage.");
             userInput = getInput("What class do you want?");
             setClass(userInput);
         }
     }
+
     public static void setClass(String className){
         if (className.equalsIgnoreCase("warrior")){
             User user = new User(25,25,50,15);
@@ -47,11 +55,13 @@ public class Main{
             setClass = true;
         }
     }
+
     public static String getInput(String message){
         System.out.println(message);
         String returnMessage = input.nextLine();
         return returnMessage;
     }
+
     public static void checkInput(String userInput){
         if (userInput.equalsIgnoreCase("quit")){
             quit();
@@ -63,17 +73,46 @@ public class Main{
             dungeon();
         }
     }
+
     public static void quit(){
-         String areYouSure = getInput("Are you sure you want to quit?  Any unsaved data will be lost.");
-         if(areYouSure.equalsIgnoreCase("yes") || areYouSure.equalsIgnoreCase("y")){
-             RUNGAME = false;
-             System.out.println("Thanks for playing!");
-         }
+        String areYouSure = getInput("Are you sure you want to quit?  Any unsaved data will be lost.");
+        if(areYouSure.equalsIgnoreCase("yes") || areYouSure.equalsIgnoreCase("y")){
+            RUNGAME = false;
+            System.out.println("Thanks for playing!");
+        }
     }
+
     public static void shop(){
 
-    }    
+    }  
+    // 4 rooms - filled with treasure, weapons, potions, or monsters
+
     public static void dungeon(){
-        
-    }    
+        // ask user for action
+        System.out.println("You walk into the dungeon, infront of you are "+
+            numberOfRooms + " rooms.");
+        System.out.println("What room do you want to explore? (Type a number)");
+        int userRoomNumber = input.nextInt();
+        // defualt room 1 (needs to be random eventually)
+        if(userRoomNumber == 1){
+            System.out.println("You walk into the first room, a monster attacks you!");
+            fightState = true;
+        }
+        // defualt room 2 (needs to be random eventually)
+        if(userRoomNumber == 2){
+            System.out.println("You walk into the second room, a chest sits on the ground, but it's locked...");
+
+        }
+        // defualt room 3 (needs to be random eventually)
+        if(userRoomNumber == 3){
+            System.out.println("You walk into the third room, you see a weapon on the ground");
+            
+        }
+        // defualt room 4 (needs to be random eventually)
+        if(userRoomNumber == 4){
+            System.out.println("You walk into the fourth room, there's nothing here");
+
+        }
+    }   
+
 }
