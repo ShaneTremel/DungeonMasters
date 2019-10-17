81713afd246ff4d331dc49eb79b40d110958d26c
Solomon <52049917+uiyotp@users.noreply.github.com>
2019-10-17 15:57:40 -0500

    Added Save File Writer
    Still need to implement in main

---
+++ SaveFileWriter.java
@@ -0,0 +1,18 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class SaveFileWriter{
+    public static void readFromFile(String path, User user){
+        BufferedWriter writer = null;
+        try{ 
+            writer = new BufferedWriter(new FileWriter(path, false)); 
+            String line = String.join(",",Integer.toString(user.getMaxHP()),Integer.toString(user.getCurrentHP()),Integer.toString(user.getAttackPower()),
+                Integer.toString(user.getRunSpeed()),Integer.toString(user.getGold()));
+            writer.write(line);
+            writer.close();
+        }
+        catch(IOException e){System.out.println("Couldn't save file.");}
+    }
+}
