996b34b5efd3fb46434218a5b6352e33065e2764
NGoth16 <55708310+NGoth16@users.noreply.github.com>
2019-10-23 17:21:21 -0500

    Added shop generation method (currently broken)

--- Shop.java
+++ Shop.java
@@ -1,33 +1,35 @@
 import java.util.*;
 import java.io.*;
-public class Shop 
+import java.Weapons.*;
+public class Shop extends Weapons
 {
-    private static int numItems = 3;
-    private int itemCost = 0;
-    private String shopName = "";
-
-    public static Weapons[] setShop()
+    private static int numItems;
+    private static Weapons weapon = new Weapons("Sword", 1, 0, 0, 0);
+    
+    //Generates shop item
+    public Shop()
     {
-        Weapons[] shop = new Weapons[numItems];
-
-        Weapons shopWeaponTemp = new Weapons("Name", 0, 0, 0, 0);
-        
-        for(int i = 0; i < shop.length; i++)
-        {
-            if(shopWeaponTemp.getDamage() != 0)
-            {
-                shopWeaponTemp.resetStats();
-                shopWeaponTemp.setStats();
-                shop[i] = shopWeaponTemp;
-            }
-            else
-            {
-                shopWeaponTemp.setStats();
-                shop[i] = shopWeaponTemp;
-            }
-        }
-
-        return shop;
+        this.weapon = weapon;
     }
+    
+    public void setShop(Weapons[] shop)
+    {
+        Weapons shopWeaponTemp = new Weapons("", 0, 0, 0, 0);
 
+        shopWeaponTemp.setStats();
+        shop[0].setWeapon(shopWeaponTemp);
+        shopWeaponTemp.resetStats();
+        shopWeaponTemp.setStats();
+        shop[1].setWeapon(shopWeaponTemp);
+        shopWeaponTemp.resetStats();
+        shopWeaponTemp.setStats();
+        shop[2].setWeapon(shopWeaponTemp);
+        
+    }
+    
+    public Weapons setWeapon(Weapons myWeapon)
+    {
+        return myWeapon;
+    }
+    
 }
