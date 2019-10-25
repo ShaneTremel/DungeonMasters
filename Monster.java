982e37be15314e8f9ace30d73e82af5a94d63d94
Shane Tremel <shanetremel24@yahoo.com>
2019-10-24 21:14:13 -0500

    Add files via upload

--- Monster.java
+++ Monster.java
@@ -1,15 +1,25 @@
-
+import java.util.Random;
 public class Monster{
     private static int[] pickMoster = new int[]{1,2,3,4};
     private static String[] monsterType = new String[]{"spider","goblin","beast","golem" };
     private static int[] maxHP = new int[]{75,50,120,300};
     private static int[] currentHP = new int[]{75,50,120,300};
     private static int[] attackPower = new int[]{7,5,13,5};
-    public Monster(String monsterType[], int maxHP[], int currentHP[],int attackPower[]){
-        this.monsterType = monsterType;
-        this.maxHP = maxHP;
-        this.currentHP = currentHP;
-        this.attackPower = attackPower;
+
+    
+    public Monster(int r){
+        String ranMonsterType;
+        ranMonsterType = monsterType[r];
+        
+        int ranMaxHP;
+        ranMaxHP = maxHP[r];
+        
+        int ranCurrentHP;
+        ranCurrentHP = currentHP[r];
+        
+        int ranAttackPower;
+        ranAttackPower = attackPower[r];
+        System.out.println(ranMonsterType+ ","+ranMaxHP+","+ranCurrentHP+","+ranAttackPower);
     }
     
     // getters and setters
